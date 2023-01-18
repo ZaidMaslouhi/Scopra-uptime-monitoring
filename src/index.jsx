4f55d1edafa4b3ccdafc2fcbbfe5155573b2d1c3
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-01-18 01:19:40 +0100

    Add and confg ESLint

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
