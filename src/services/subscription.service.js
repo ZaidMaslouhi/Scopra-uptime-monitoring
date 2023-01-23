7f5d1bcffe6c1958c027361a08d4729ae14a2749
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-01-23 23:49:35 +0100

    Rename firebase config file

--- src/services/subscription.service.js
+++ src/services/subscription.service.js
@@ -1,5 +1,5 @@
 import { collection, addDoc } from "firebase/firestore";
-import { database } from "./firebase.service";
+import { database } from "../firebase.config";
 
 export const subscribe = async (subscriber) => {
   const ref = collection(database, "subscribers");
