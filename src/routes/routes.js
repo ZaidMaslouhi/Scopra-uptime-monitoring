063e6851dd0c99efda5dd19ccd6d65ec5ca38b3f
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-01-28 21:47:26 +0100

    Add routes of sign in/sign up pages

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,12 @@
 import React from "react";
 import GetInTouch from "../pages/GetInTouch/GetInTouch";
 import NotFound from "../pages/NotFound/NotFound";
+import Login from "../pages/Auth/Login/Login";
+import Registration from "../pages/Auth/Registration/Registration";
 
 export const routes = [
-  { key: 1, path: "/", element: <GetInTouch />, exact: true },
-  { key: 2, path: "*", element: <NotFound /> },
+  { key: 1, path: "*", element: <NotFound /> },
+  { key: 2, path: "/", element: <GetInTouch />, exact: true },
+  { key: 3, path: "/login", element: <Login /> },
+  { key: 4, path: "/register", element: <Registration /> },
 ];
