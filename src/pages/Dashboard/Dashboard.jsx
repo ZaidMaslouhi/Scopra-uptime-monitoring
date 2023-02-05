c6320123b577d4685dd7395987f5f7f749e511b2
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-02-05 22:14:24 +0100

    Add Dashboard Page and route to it

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <main className='w-full h-screen bg-slate-100 flex justify-center items-center'>
+        <h1 className='text-4xl font-semibold text-slate-900'>Welcome</h1>
+    </main>
+  )
+}
+
+export default Dashboard
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,12 @@ import GetInTouch from "../pages/GetInTouch/GetInTouch";
 import NotFound from "../pages/NotFound/NotFound";
 import Login from "../pages/Auth/Login/Login";
 import Register from "../pages/Auth/Registration/Register";
+import Dashboard from "../pages/Dashboard/Dashboard";
 
 export const routes = [
   { key: 1, path: "*", element: <NotFound /> },
   { key: 2, path: "/", element: <GetInTouch />, exact: true },
   { key: 3, path: "/login", element: <Login /> },
   { key: 4, path: "/register", element: <Register /> },
+  { key: 5, path: "/dashboard", element: <Dashboard /> },
 ];
