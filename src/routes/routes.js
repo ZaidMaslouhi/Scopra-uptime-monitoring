2c4848dd368648e9f5191d2cd86994b93bb8086c
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-02-05 22:11:49 +0100

    Rename Registration Page to Register

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,11 @@ import React from "react";
 import GetInTouch from "../pages/GetInTouch/GetInTouch";
 import NotFound from "../pages/NotFound/NotFound";
 import Login from "../pages/Auth/Login/Login";
-import Registration from "../pages/Auth/Registration/Registration";
+import Register from "../pages/Auth/Registration/Register";
 
 export const routes = [
   { key: 1, path: "*", element: <NotFound /> },
   { key: 2, path: "/", element: <GetInTouch />, exact: true },
   { key: 3, path: "/login", element: <Login /> },
-  { key: 4, path: "/register", element: <Registration /> },
+  { key: 4, path: "/register", element: <Register /> },
 ];
