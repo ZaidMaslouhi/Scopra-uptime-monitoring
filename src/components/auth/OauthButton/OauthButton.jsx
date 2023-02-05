82ff2c81ce32f152e161407aae3984fad9edbbfd
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-02-05 22:15:12 +0100

    Create component OauthButton

---
+++ src/components/auth/OauthButton/OauthButton.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function OauthButton({ title, Icon, handleClick, routeTo }) {
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleButtonClick = async () => {
+    try {
+      await handleClick();
+      navigate(routeTo);
+    } catch (error) {
+      setErrorMessage("Unable to get profile information from the provider");
+    }
+  };
+  return (
+    <>
+      <button
+        type="button"
+        className="text-center text-slate-800 border-2 border-slate-800 bg-white font-semibold block w-full rounded-md mt-3 px-4 py-2 shadow-sm transition-all duration-200 outline-none hover:shadow-md"
+        onClick={handleButtonClick}
+      >
+        <Icon className="inline text-xl mr-1" /> {title}
+      </button>
+      <span className="mt-2 text-sm text-center text-red-700 leading-4 block">
+        {errorMessage}
+      </span>
+    </>
+  );
+}
+
+OauthButton.propTypes = {
+  Icon: PropTypes.elementType.isRequired,
+  title: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  routeTo: PropTypes.string.isRequired,
+};
+
+export default OauthButton;
