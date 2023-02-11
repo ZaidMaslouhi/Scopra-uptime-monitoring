88f476b9935ff7dcc844e3ae6ce9e69f50e2269b
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-02-11 21:30:50 +0100

    Add Loading state/animation: -Add loading state to useAuth custom hook -Add loading animation to protected route

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,13 +3,20 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase.config";
 
 function useAuth() {
-  const [currentUser, setCurrentUser] = useState({});
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loadingCurrentUser, setLoadingCurrentUser] = useState(true);
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => setCurrentUser(user));
+    const unlisten = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoadingCurrentUser(false);
+    });
+    return () => {
+      unlisten();
+    };
   }, [currentUser]);
 
-  return { currentUser };
+  return { currentUser, loadingCurrentUser };
 }
 
 export default useAuth;
--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -1,11 +1,25 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
 import useAuth from "../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
+import loading from "../assets/lotties/loading.json";
+import Lottie from "lottie-react";
 
 function ProtectedRoute({ children }) {
-  const { currentUser } = useAuth();
-  const navigate = useNavigate();
-
-  return currentUser ? children : navigate("/login");
+  const { currentUser, loadingCurrentUser } = useAuth();
+  return loadingCurrentUser ? (
+    <div className="w-full h-screen flex justify-center items-center bg-slate-200 ">
+      <Lottie animationData={loading} loop={true} className="w-64 h-64" />
+    </div>
+  ) : currentUser ? (
+    children
+  ) : (
+    <Navigate to="/login" />
+  );
 }
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default ProtectedRoute;
