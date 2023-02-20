e1577610303ed815078a090ef741ec5fd6950c4b
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-02-20 21:19:07 +0100

    Write tests for the onboarding component

---
+++ src/pages/onBoarding/__tests__/OnBoarding.test.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
+import OnBoarding from "../OnBoarding";
+import { newProject } from "../../../services/project.service";
+import {
+  successNotification,
+  ErrorNotification,
+} from "../../../components/toasts/toasts";
+import { useNavigate } from "react-router-dom";
+
+jest.mock("../../../services/project.service", () => ({
+  newProject: jest.fn(),
+}));
+
+jest.mock("../../../components/toasts/toasts", () => ({
+  successNotification: jest.fn(),
+  ErrorNotification: jest.fn(),
+}));
+
+jest.mock("react-router-dom", () => ({
+  useNavigate: jest.fn(),
+}));
+
+describe("OnBoarding component", () => {
+  test("renders project name input and create button", () => {
+    render(<OnBoarding />);
+    expect(screen.getByText(/Welcome to Scopra/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Project name/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /Create a new project/i })
+    ).toBeInTheDocument();
+  });
+
+  test("creates new project on form submission", async () => {
+    newProject.mockResolvedValueOnce();
+    render(<OnBoarding />);
+
+    const projectNameInput = screen.getByPlaceholderText(/Project name/i);
+    const createButton = screen.getByRole("button", {
+      name: /Create a new project/i,
+    });
+
+    fireEvent.change(projectNameInput, {
+      target: { value: "Test project Name" },
+    });
+    fireEvent.click(createButton);
+
+    await waitFor(() => {
+      expect(newProject).toHaveBeenCalledWith(expect.any(Object), {
+        name: "Test project Name",
+        timestamp: expect.any(Number),
+      });
+    });
+  });
+
+  test("shows success notification on project creation", async () => {
+    newProject.mockResolvedValueOnce();
+    render(<OnBoarding />);
+
+    const projectNameInput = screen.getByPlaceholderText(/Project name/i);
+    const createButton = screen.getByRole("button", {
+      name: /Create a new project/i,
+    });
+
+    fireEvent.change(projectNameInput, {
+      target: { value: "Test project Name" },
+    });
+    fireEvent.click(createButton);
+
+    await waitFor(() => {
+      expect(successNotification).toHaveBeenCalledWith("New project created.");
+    });
+  });
+
+  test("shows error notification on project creation failure", async () => {
+    newProject.mockRejectedValueOnce(new Error("Unable to create project"));
+    render(<OnBoarding />);
+
+    const projectNameInput = screen.getByPlaceholderText(/Project name/i);
+    const createButton = screen.getByRole("button", {
+      name: /Create a new project/i,
+    });
+
+    fireEvent.change(projectNameInput, {
+      target: { value: "Test project Name" },
+    });
+    fireEvent.click(createButton);
+
+    await waitFor(() => {
+      expect(ErrorNotification).toHaveBeenCalledWith(
+        "Error: Unable to create a new project."
+      );
+    });
+  });
+
+  test("Navigates to monitor page on form submission", async () => {
+    const navigate = jest.fn();
+    useNavigate.mockImplementation(() => navigate);
+    newProject.mockResolvedValueOnce();
+    render(<OnBoarding />);
+
+    const projectNameInput = screen.getByPlaceholderText(/Project name/i);
+    const createButton = screen.getByRole("button", {
+      name: /Create a new project/i,
+    });
+
+    fireEvent.change(projectNameInput, {
+      target: { value: "Test project Name" },
+    });
+    fireEvent.click(createButton);
+
+    await waitFor(() => {
+      expect(navigate).toBeCalledWith("/monitors");
+    });
+  });
+});
