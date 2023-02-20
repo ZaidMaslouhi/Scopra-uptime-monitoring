b77a54e1e4b5fb7a482f22240ced642593c7c1fc
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-02-20 19:47:07 +0100

    Add loading state/animation to the protected route

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -2,15 +2,12 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import useAuth from "../hooks/useAuth";
-import loading from "../assets/lotties/loading.json";
-import Lottie from "lottie-react";
+import LoadingAnimation from "../components/loading/LoadingAnimation";
 
 function ProtectedRoute({ children }) {
   const { currentUser, loadingCurrentUser } = useAuth();
   return loadingCurrentUser ? (
-    <div className="w-full h-screen flex justify-center items-center bg-slate-200 ">
-      <Lottie animationData={loading} loop={true} className="w-64 h-64" />
-    </div>
+    <LoadingAnimation />
   ) : currentUser ? (
     children
   ) : (
