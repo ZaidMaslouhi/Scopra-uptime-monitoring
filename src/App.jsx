6afa914ffd39bd91174102eb540892c323e303fe
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-02-20 19:45:19 +0100

    Add Toaster component to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { routes } from "./routes/routes";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="App">
+      <Toaster />
       <Routes>
         {routes &&
           routes.map((route) => {
