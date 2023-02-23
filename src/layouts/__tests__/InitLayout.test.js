4c1c81c7b7f8025bbbe0b6c91adf87e4e56e6031
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-02-23 21:30:25 +0100

    Write tests for InitLayout

---
+++ src/layouts/__tests__/InitLayout.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import InitLayout from "../InitLayout";
+
+describe("InitLayout", () => {
+  const image = null;
+  const children = <div>Test Content</div>;
+
+  it("renders the children prop", () => {
+    render(<InitLayout image={image}>{children}</InitLayout>);
+    expect(screen.getByText("Test Content")).toBeInTheDocument();
+  });
+
+  it("renders the Scopra logo", () => {
+    render(<InitLayout image={image}>{children}</InitLayout>);
+    expect(screen.getByAltText("scopra logo")).toBeInTheDocument();
+  });
+
+  it("renders the Lottie animation", () => {
+    render(<InitLayout image={image}>{children}</InitLayout>);
+    expect(screen.getByRole("img")).toBeInTheDocument();
+  });
+});
