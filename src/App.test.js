fa247ff9d4efd82e7da70b15359d4bf0465cbd59
Zaid Elmaslouhi <66847304+ZaidMaslouhi@users.noreply.github.com>
2023-03-15 23:57:04 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,6 @@ import GetInTouch from "./pages/GetInTouch/GetInTouch";
 jest.mock("./services/subscription.service", () => ({
   subscribe: jest.fn(),
 }));
-jest.mock("./services/auth.service", () => ({
-  getCurrentUser: jest.fn(),
-}));
 
 describe("App", () => {
   afterEach(cleanup);
