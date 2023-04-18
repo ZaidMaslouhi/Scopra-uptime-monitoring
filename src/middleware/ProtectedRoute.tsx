049dd3f247c1ddff0623eab9e04c338b70fcb01c
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-04-18 18:55:36 +0000

    feat: create ProtectedRoute.tsx

---
+++ src/middleware/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { selectUserState } from "../store/slices/auth.slice";
+import { useAppSelector } from "../utils/hooks/react-redux-hooks";
+import LoadingAnimation from "../components/ui/loading/LoadingAnimation";
+
+function ProtectedRoute({ children }: { children: JSX.Element }): JSX.Element {
+  const user = useAppSelector(selectUserState);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user.status !== "Pending" && !user.user) navigate("/login");
+  }, [user.status, user.user]);
+
+  return user.status === "Pending" ? <LoadingAnimation /> : children;
+}
+
+export default ProtectedRoute;
