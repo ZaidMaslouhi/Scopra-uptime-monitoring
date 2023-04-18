e0b49e61037c546af3f60963a065692e363ec045
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-04-18 18:51:23 +0000

    move and rename Usage.jsx : to src/components/ui/account/Usage.tsx

--- src/components/ui/account/Usage.tsx
+++ src/components/ui/account/Usage.tsx
@@ -1,13 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BiInfinite } from "react-icons/bi";
+import { RootState } from "../../../store/store";
+import { useAppSelector } from "../../../utils/hooks/react-redux-hooks";
 
 function Usage() {
+  const projects = useAppSelector((state: RootState) => state.projects);
+  const monitors = useAppSelector((state: RootState) => state.monitors);
 
-  useEffect(() => {
-    console.log("Usage");
-  }, []);
-
-  
   return (
     <section className="w-full h-full">
       <div className="w-full flex items-center">
@@ -16,22 +15,24 @@ function Usage() {
         </p>
       </div>
       <hr className="my-6" />
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-wrap gap-10">
         {/* Monitors */}
-        <div className="flex flex-col gap-2 w-1/2">
+        <div className="w-full flex flex-col basis-5/12 gap-2">
           <div className="flex justify-between">
             <p className="text-lg font-medium">Monitors</p>
-            <p className="text-lg font-semibold">5/15</p>
+            <p className="text-lg font-semibold">
+              {monitors.monitors.length}/15
+            </p>
           </div>
           <div className="w-full bg-slate-200 rounded-full h-2.5">
             <div
               className="bg-slate-600 h-2.5 rounded-full"
-              style={{ width: `${33}%` }}
+              style={{ width: `${(monitors.monitors.length / 15) * 100}%` }}
             ></div>
           </div>
         </div>
         {/* Status Pages */}
-        <div className="flex flex-col gap-2 w-1/2">
+        <div className="w-full flex flex-col basis-5/12 gap-2">
           <div className="flex justify-between">
             <p className="text-lg font-medium">Status Pages</p>
             <p className="text-lg font-semibold">0/1</p>
@@ -44,7 +45,7 @@ function Usage() {
           </div>
         </div>
         {/* Teammates */}
-        <div className="flex flex-col gap-2 w-1/2">
+        <div className="w-full flex flex-col basis-5/12 gap-2">
           <div className="flex justify-between">
             <p className="text-lg font-medium">Teammates</p>
             <p className="text-lg font-semibold">1/3</p>
@@ -57,7 +58,7 @@ function Usage() {
           </div>
         </div>
         {/* SMS credits */}
-        <div className="flex flex-col gap-2 w-1/2">
+        <div className="w-full flex flex-col basis-5/12 gap-2">
           <div className="flex justify-between">
             <p className="text-lg font-medium">SMS credits</p>
             <p className="text-lg font-semibold">20/20</p>
@@ -70,7 +71,7 @@ function Usage() {
           </div>
         </div>
         {/* Status page subscribers */}
-        <div className="flex flex-col gap-2 w-1/2">
+        <div className="w-full flex flex-col basis-5/12 gap-2">
           <div className="flex justify-between">
             <p className="text-lg font-medium">Status page subscribers</p>
             <p className="text-lg font-semibold">0/10</p>
@@ -83,22 +84,21 @@ function Usage() {
           </div>
         </div>
         {/* Projects */}
-        <div className="flex flex-col gap-2 w-1/2">
+        <div className="w-full flex flex-col basis-5/12 gap-2">
           <div className="flex justify-between">
             <p className="text-lg font-medium">Projects</p>
             <p className="text-lg font-semibold">
-              1/
+              {projects.projects.length}/
               <BiInfinite className="inline ml-1" />
             </p>
           </div>
           <div className="w-full bg-slate-200 rounded-full h-2.5">
             <div
               className="bg-slate-600 h-2.5 rounded-full"
-              style={{ width: `${1}%` }}
+              style={{ width: `2%` }}
             ></div>
           </div>
         </div>
-        {/*  */}
       </div>
     </section>
   );
