493bc5a7df0302ad3c5f6e5dc0dbe7ad2aa2e7d7
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-04-18 19:35:22 +0000

    move and rename BillingPlans.jsx : to src/utils/constants/BillingPlans.tsx

--- src/utils/constants/BillingPlans.tsx
+++ src/utils/constants/BillingPlans.tsx
@@ -1,30 +1,45 @@
 import React from "react";
 import { CgInfinity } from "react-icons/cg";
-
-
 const infinity = <CgInfinity />;
 
-export const BillingPlans = [
+export interface Plan {
+  name: string;
+  properties: PlanProperties;
+  price: number;
+  period: "month" | "year";
+  currency: "$";
+  preferred?: boolean;
+}
+
+interface PlanProperties {
+  Monitors: number | JSX.Element;
+  Frequency: string;
+  "Status Pages": number | JSX.Element;
+  SMS: number | "-";
+  Teammates: number | "-";
+  Subscribers?: number | JSX.Element;
+}
+
+export const BillingPlans: Plan[] = [
   {
     name: "Free",
     properties: {
       Monitors: 5,
       Frequency: "3min",
-      "Status pages": 1,
+      "Status Pages": 1,
       SMS: "-",
       Teammates: "-",
     },
     price: 0,
     period: "month",
     currency: "$",
-    preferred: true,
   },
   {
     name: "Hobby",
     properties: {
       Monitors: 15,
       Frequency: "30Sec",
-      "Status pages": 1,
+      "Status Pages": 1,
       SMS: 5,
       Teammates: "-",
     },
@@ -37,20 +52,21 @@ export const BillingPlans = [
     properties: {
       Monitors: 50,
       Frequency: "30Sec",
-      "Status pages": 1,
+      "Status Pages": 1,
       SMS: 20,
       Teammates: 1,
     },
     price: 29,
     period: "month",
     currency: "$",
+    preferred: true,
   },
   {
     name: "Pro",
     properties: {
       Monitors: 100,
       Frequency: "30Sec",
-      "Status pages": infinity,
+      "Status Pages": infinity,
       SMS: 75,
       Teammates: 5,
       Subscribers: 1000,
@@ -64,7 +80,7 @@ export const BillingPlans = [
     properties: {
       Monitors: infinity,
       Frequency: "10Sec",
-      "Status pages": infinity,
+      "Status Pages": infinity,
       SMS: 250,
       Teammates: 15,
       Subscribers: infinity,
