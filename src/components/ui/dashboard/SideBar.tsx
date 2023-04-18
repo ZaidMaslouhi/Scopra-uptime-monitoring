f600b2e8539a06454c27873eb0cc673582d26880
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-04-18 18:58:01 +0000

    move and rename SideBar.jsx : to src/components/ui/dashboard/SideBar.tsx

--- src/components/ui/dashboard/SideBar.tsx
+++ src/components/ui/dashboard/SideBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { sideBarItems } from "../../data/sideBarItems";
+import { sideBarItems } from "../../../utils/constants/sideBarItems";
 import { NavLink } from "react-router-dom";
+import ComingSoon from "./ComingSoon";
 
 function SideBar() {
   return (
-    <aside className="w-1/5 2xl:w-1/6 h-full flex flex-col justify-between text-2xl font-lato font-normal overflow-y-auto">
-      <ul className="flex flex-col flex-1">
+    <aside className="w-1/5 2xl:w-1/6 min-h-full h-auto flex flex-col justify-between text-2xl font-lato font-normal overflow-y-auto">
+      <ul className="fixed flex flex-col flex-1" style={{ width: "inherit" }}>
         {sideBarItems.length > 0 &&
           sideBarItems.map((item) => (
             <React.Fragment key={item.key}>
@@ -26,11 +27,7 @@ function SideBar() {
                 >
                   {item.icon}
                   <span className="ml-6">{item.title}</span>
-                  {item.comingSoon && (
-                    <span className="ml-auto p-2 bg-purple-100 text-xs text-purple-800 font-medium border border-purple-500 rounded-full">
-                      Coming Soon
-                    </span>
-                  )}
+                  {item.comingSoon && <ComingSoon />}
                 </NavLink>
               </li>
             </React.Fragment>
