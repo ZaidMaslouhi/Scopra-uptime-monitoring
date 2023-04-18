5e7be7844a7369416bb4de568e2e55d70c63e351
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-04-18 18:45:34 +0000

    chore: move and rename MessageReponse.jsx : to src/components/ui/getInTouch/MessageReponse/MessageReponse.tsx

--- src/components/ui/getInTouch/MessageReponse/MessageReponse.tsx
+++ src/components/ui/getInTouch/MessageReponse/MessageReponse.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function MessageReponse({ message, className }) {
+function MessageReponse({
+  message,
+  className,
+}: {
+  message: string;
+  className: string;
+}) {
   return (
     <div className={`px-5 py-3 border-2 rounded-md ${className}`}>
       <p className="text-sm">{message}</p>
     </div>
   );
 }
-
-MessageReponse.propTypes = {
-  message: PropTypes.string.isRequired,
-  className: PropTypes.string,
-};
 
 export default MessageReponse;
