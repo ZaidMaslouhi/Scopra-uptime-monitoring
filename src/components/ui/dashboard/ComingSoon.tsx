5719583106a73da36cbe01632918505bfcd7e8b6
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-04-18 18:57:26 +0000

    feat: create ComingSoon.tsx

---
+++ src/components/ui/dashboard/ComingSoon.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ComingSoon() {
+  return (
+    <span className="ml-auto p-2 bg-purple-100 text-xs text-purple-800 font-medium border border-purple-500 rounded-full">
+      Coming Soon
+    </span>
+  );
+}
+
+export default ComingSoon;
