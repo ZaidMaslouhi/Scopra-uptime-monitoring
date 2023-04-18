70a91628d6d598c338e5f3b6a8e83a0265363a4a
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-04-18 18:49:45 +0000

    move and rename ProjectsButton.jsx : to src/components/ui/dashboard/ProjectsButton.tsx

--- src/components/ui/dashboard/ProjectsButton.tsx
+++ src/components/ui/dashboard/ProjectsButton.tsx
@@ -1,32 +1,35 @@
-import React, { useContext, useEffect, useState } from "react";
-import { ProjectsContext } from "../../context/ProjectsContext";
+import React, { useState } from "react";
 import { HiOutlineChevronUpDown } from "react-icons/hi2";
 import AddProjectModal from "./AddProjectModal";
 import { IoIosAddCircleOutline } from "react-icons/io";
+import { Project } from "../../../interfaces/project.interface";
+import {
+  selectCurrentProject,
+  selectProjectsState,
+  setCurrentProject,
+} from "../../../store/slices/projects.slice";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../../utils/hooks/react-redux-hooks";
 
 function ProjectsButton() {
-  const { projects, setProjects } = useContext(ProjectsContext);
   const [showModal, setShowModal] = useState(false);
-  const [currentProject, setCurrentProject] = useState(
-    projects.find((project) => project.selected)
-  );
-
-  useEffect(() => {
-    setProjects((prevProject) => {
-      return prevProject.map((project) => {
-        project.selected = project.id === currentProject.id;
-        return project;
-      });
-    });
-  }, [currentProject]);
+  const projects = useAppSelector(selectProjectsState);
+  const currentProject = useAppSelector(selectCurrentProject) as Project;
+  const dispatch = useAppDispatch();
 
   const closeModal = () => {
     setShowModal(false);
   };
 
+  const selectProject = (projectId: string) => {
+    dispatch(setCurrentProject({ id: projectId }));
+  };
+
   return (
     <>
-      {projects.length > 0 && (
+      {projects.projects.length > 0 && (
         <div className="max-w-xs w-52  relative text-center">
           {showModal ? <AddProjectModal onClose={closeModal} /> : null}
           <button
@@ -38,13 +41,13 @@ function ProjectsButton() {
             <HiOutlineChevronUpDown className="inline ml-auto mr-0 text-slate-800" />
           </button>
           <ul className="w-5/6 bg-slate-50 border border-slate-300 rounded-lg hidden overflow-hidden shadow-lg absolute right-2/4 translate-x-1/2 peer-hover:block hover:block">
-            {projects.map((project) => {
+            {projects.projects.map((project: Project) => {
               if (!project.selected)
                 return (
                   <li
-                    className="px-6 py-4 text-md font-medium text-slate-800 flex items-center justify-center cursor-pointer hover:bg-slate-300"
+                    className={`${project.id} px-6 py-4 text-md font-medium text-slate-800 flex items-center justify-center cursor-pointer hover:bg-slate-300`}
                     key={project.id}
-                    onClick={() => setCurrentProject(project)}
+                    onClick={() => selectProject(project.id)}
                   >
                     {project.name}
                   </li>
