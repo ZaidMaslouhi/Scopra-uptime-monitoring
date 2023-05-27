805c87ff7c55a593979e788feddf3fa174c47f69
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-27 14:12:24 +0100

    fix: SENTRY_DSN is undefined

--- user-service/src/config/envirement.js
+++ user-service/src/config/envirement.js
@@ -25,5 +25,7 @@ module.exports = {
   // Github auth
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL
+  GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL,
+  // Sentry Data Source Name
+  SENTRY_DSN: process.env.SENTRY_DSN
 }
