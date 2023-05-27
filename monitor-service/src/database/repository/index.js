40f5b329e3f0399e7a7ebf4a5415791f721c334e
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-27 20:43:59 +0100

    feat: add monitor repository

---
+++ monitor-service/src/database/repository/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MonitorRepository: require('./monitor-repository')
+}
---
+++ monitor-service/src/database/repository/monitor-repository.js
@@ -0,0 +1,54 @@
+const { MonitorModel } = require('../models')
+
+class MonitorRepository {
+  async FindMonitorsByProjectId (projectId) {
+    try {
+      const monitors = await MonitorModel.find({ projectId })
+
+      return monitors ?? []
+    } catch (_) {
+      throw new Error('Unable to find monitor')
+    }
+  }
+
+  async CreateNewMonitor ({ name, taskId, uri, projectId }) {
+    try {
+      const newMonitor = await MonitorModel.create({
+        name,
+        taskId,
+        uri,
+        projectId
+      })
+
+      return !newMonitor ? {} : newMonitor.toJSON()
+    } catch (_) {
+      throw new Error('Unable to create new monitor')
+    }
+  }
+
+  async UpdateMonitor (monitor) {
+    try {
+      const updatedMonitor = await MonitorModel.findByIdAndUpdate(
+        monitor.id,
+        { name: monitor.name },
+        { new: true }
+      )
+
+      return !updatedMonitor ? {} : updatedMonitor.toJSON()
+    } catch (_) {
+      throw new Error('Unable to update monitor')
+    }
+  }
+
+  async DeleteMonitorById (monitor) {
+    try {
+      const deletedMonitor = await MonitorModel.findByIdAndRemove(monitor.id)
+
+      return !deletedMonitor ? {} : deletedMonitor.toJSON()
+    } catch (_) {
+      throw new Error('Unable to remove monitor')
+    }
+  }
+}
+
+module.exports = MonitorRepository
