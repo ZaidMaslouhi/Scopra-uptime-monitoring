834ed7dd2a26a4a367e8c57f1502e6634f0f262b
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-28 00:22:19 +0100

    test: add mocks for redis and websocket server

--- monitor-service/src/__mocks__/mocks.js
+++ monitor-service/src/__mocks__/mocks.js
@@ -1,3 +1,20 @@
+jest.mock('redis', () => {
+  return {
+    createClient: () => {
+      return {
+        connect: jest.fn(),
+        subscribe: jest.fn(),
+        publish: jest.fn()
+      }
+    }
+  }
+})
+
+jest.mock('ws', () => {
+  return {
+    WebSocketServer: jest.fn()
+  }
+})
 
 jest.mock('node-cron', () => ({
   schedule: jest.fn()
