2cc036e4a1e66c184752efe32ea1491b75584d3b
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-27 13:07:00 +0100

    feat: add cookie parser and express session

--- user-service/src/express-app.js
+++ user-service/src/express-app.js
@@ -1,14 +1,26 @@
 const cors = require('cors')
 const express = require('express')
 const { userApi } = require('./api')
+const session = require('express-session')
+const { APP_SECRET } = require('./config')
+const cookieParser = require('cookie-parser')
 
 const expressApp = () => {
   const app = express()
 
   // Middlewares
+  app.use(cookieParser())
   app.use(express.json({ limit: '1mb' }))
   app.use(cors({ credentials: true, origin: true }))
   app.use(express.urlencoded({ extended: true, limit: '1mb' }))
+  app.use(
+    session({
+      secret: APP_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: false }
+    })
+  )
 
   // Api
   userApi(app)
