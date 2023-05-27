4ce722b8044e4fc22af4774876637c99f8edb7eb
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-28 00:55:15 +0100

    chore: add RPC events & channels

--- project-service/src/config/index.js
+++ project-service/src/config/index.js
@@ -1,9 +1,17 @@
 const Env = require('./envirement')
 const { RedisPublisher, RedisSubscriber } = require('./redis')
+const { UserServiceEvents, ProjectServiceEvents } = require('./rpcEvents')
 
 module.exports = {
   ...Env,
   // Redis
   RedisPublisher,
-  RedisSubscriber
+  RedisSubscriber,
+  // RPC Services
+  USER_SERVICE: 'USER_SERVICE',
+  PROJECT_SERVICE: 'PROJECT_SERVICE',
+  MONITOR_SERVICE: 'MONITOR_SERVICE',
+  // RPC Events
+  UserServiceEvents,
+  ProjectServiceEvents
 }
---
+++ project-service/src/config/rpcEvents.js
@@ -0,0 +1,10 @@
+const UserServiceEvents = {
+  SET_DEFAULT_PROJECT: 'SET_DEFAULT_PROJECT'
+}
+
+const ProjectServiceEvents = {
+  ADD_MONITOR_TO_PROJECT: 'ADD_MONITOR_TO_PROJECT',
+  DELETE_MONITOR_FROM_PROJECT: 'DELETE_MONITOR_FROM_PROJECT'
+}
+
+module.exports = { UserServiceEvents, ProjectServiceEvents }
