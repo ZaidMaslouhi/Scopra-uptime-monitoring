2287081f19de39b7feaf70dbf2e8fae1b73fe7b7
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-27 19:05:35 +0100

    feat: implement GET projects route

--- project-service/src/api/project.js
+++ project-service/src/api/project.js
@@ -1,8 +1,20 @@
+const { ProjectService } = require('../services')
 const UserAuth = require('./middlewares/auth')
 
 module.exports = (app) => {
+  const service = new ProjectService()
+
   // Get user's projects
   app.get('/', UserAuth, async (req, res, next) => {
+    try {
+      const { user: userId } = req.user
+
+      const { projects } = await service.getProjects(userId)
+
+      return res.status(200).json({ projects })
+    } catch (error) {
+      next(error)
+    }
   })
 
   // Add new project
