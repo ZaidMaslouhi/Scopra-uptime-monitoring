6e9943e981e2d565f0f3f7992cfe7ec892b7bb9b
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-27 19:01:52 +0100

    feat: define api routes

---
+++ project-service/src/api/project.js
@@ -0,0 +1,18 @@
+
+module.exports = (app) => {
+  // Get user's projects
+  app.get('/', async (req, res, next) => {
+  })
+
+  // Add new project
+  app.post('/', async (req, res, next) => {
+  })
+
+  // Update project
+  app.put('/', async (req, res, next) => {
+  })
+
+  // Delete project
+  app.delete('/:id', async (req, res, next) => {
+  })
+}
