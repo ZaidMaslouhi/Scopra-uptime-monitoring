6c0095e32d450a21abe3cb9ab68ca6de695baadd
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-27 20:55:37 +0100

    feat: add Sentry to the error handler

--- monitor-service/src/utils/error-handler/index.js
+++ monitor-service/src/utils/error-handler/index.js
@@ -1,5 +1,35 @@
+const Sentry = require('@sentry/node')
+const { SENTRY_DSN } = require('../../config')
+const {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  ConflictError
+} = require('./app-errors')
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    tracesSampleRate: 1.0
+  })
+}
 
 const ErrorHandler = (error, req, res, next) => {
+  let reportError = true;
+
+  [
+    BadRequestError,
+    UnauthorizedError,
+    ForbiddenError,
+    NotFoundError,
+    ConflictError
+  ].forEach((ErrorType) => {
+    if (error instanceof ErrorType) reportError = false
+  })
+
+  if (reportError && process.env.NODE_ENV === 'production') { Sentry.captureException(error) }
+
   const statusCode = error.statusCode || 500
   const data = error.data || error.message
   return res.status(statusCode).json(data)
