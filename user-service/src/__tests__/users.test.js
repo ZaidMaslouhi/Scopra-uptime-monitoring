82c91329c1808f310bbc7dfa0997f1ea22a31012
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-27 14:48:47 +0100

    test: add tests for sing-up route/service

--- user-service/src/__tests__/users.test.js
+++ user-service/src/__tests__/users.test.js
@@ -167,3 +167,171 @@ describe('POST /signin', () => {
     })
   })
 })
+
+describe('POST /signup', () => {
+  describe('user email or passwod not defined on the request body', () => {
+    test('should return error message with 400 status', async () => {
+      const response = await request(app).post('/signup').send({ user: {} })
+
+      expect(response.statusCode).toBe(400)
+      expect(response.text).toEqual('"User Email and Password are required!"')
+    })
+  })
+
+  describe('the given email already exist on the database', () => {
+    test('should return error message with 409 status', async () => {
+      const mockFindUserByEmail = jest
+        .spyOn(UserRepository.prototype, 'FindUserByEmail')
+        .mockResolvedValue({
+          id: userPayload.user.id,
+          email: userPayload.user.email
+        })
+
+      const response = await request(app)
+        .post('/signup')
+        .send({ user: { ...userPayload.user } })
+
+      expect(response.statusCode).toBe(409)
+      expect(response.text).toEqual('"User already exists!"')
+      expect(mockFindUserByEmail).toHaveBeenCalledTimes(1)
+      expect(mockFindUserByEmail).toHaveBeenCalledWith(userPayload.user.email)
+    })
+  })
+
+  describe('Create new user', () => {
+    test('should invoke CreateNewUser function once', async () => {
+      jest
+        .spyOn(UserRepository.prototype, 'FindUserByEmail')
+        .mockResolvedValue(null)
+
+      const mockCreateNewUser = jest
+        .spyOn(UserRepository.prototype, 'CreateNewUser')
+        .mockResolvedValue({})
+
+      await request(app)
+        .post('/signup')
+        .send({ user: { ...userPayload.user } })
+
+      const username =
+        userPayload.user.username ??
+        userPayload.user.email.slice(0, userPayload.user.email.indexOf('@'))
+
+      expect(mockCreateNewUser).toHaveBeenCalledTimes(1)
+      expect(mockCreateNewUser).toHaveBeenCalledWith({
+        email: userPayload.user.email,
+        username,
+        password: expect.any(String),
+        source: 'Scopra'
+      })
+    })
+
+    test('should update user refresh token', async () => {
+      jest
+        .spyOn(UserRepository.prototype, 'FindUserByEmail')
+        .mockResolvedValue(null)
+
+      jest
+        .spyOn(UserRepository.prototype, 'CreateNewUser')
+        .mockResolvedValue({ _id: userPayload.user.id })
+
+      const mockUpdateUserToken = jest
+        .spyOn(UserRepository.prototype, 'UpdateUserToken')
+        .mockResolvedValue({})
+
+      const response = await request(app)
+        .post('/signup')
+        .send({ user: userPayload.user })
+
+      expect(response.statusCode).toBe(201)
+      expect(mockUpdateUserToken).toHaveBeenCalledTimes(1)
+      expect(mockUpdateUserToken).toHaveBeenCalledWith({
+        id: userPayload.user.id,
+        token: expect.any(String)
+      })
+    })
+
+    test('should return error message with 500 status', async () => {
+      jest
+        .spyOn(UserRepository.prototype, 'FindUserByEmail')
+        .mockResolvedValue(null)
+
+      jest
+        .spyOn(UserRepository.prototype, 'CreateNewUser')
+        .mockResolvedValue({ _id: userPayload.user.id })
+
+      const mockUpdateUserToken = jest
+        .spyOn(UserRepository.prototype, 'UpdateUserToken')
+        .mockResolvedValue(null)
+
+      const response = await request(app)
+        .post('/signup')
+        .send({ user: { ...userPayload.user } })
+
+      expect(response.statusCode).toBe(500)
+      expect(response.text).toEqual('"Cannot update user token!"')
+      expect(mockUpdateUserToken).toHaveBeenCalledTimes(1)
+    })
+
+    test('should response user object and access toekn with 201 status', async () => {
+      jest
+        .spyOn(UserRepository.prototype, 'FindUserByEmail')
+        .mockResolvedValue(null)
+
+      jest
+        .spyOn(UserRepository.prototype, 'CreateNewUser')
+        .mockResolvedValue({ _id: userPayload.user.id })
+
+      jest
+        .spyOn(UserRepository.prototype, 'UpdateUserToken')
+        .mockResolvedValue({
+          _id: userPayload.user.id,
+          email: userPayload.user.email
+        })
+
+      const response = await request(app)
+        .post('/signup')
+        .send({ user: userPayload.user })
+
+      expect(response.statusCode).toBe(201)
+      expect(response.body).toEqual({
+        user: {
+          _id: userPayload.user.id,
+          email: userPayload.user.email,
+          token: expect.any(String)
+        }
+      })
+    })
+
+    test('should set cookie', async () => {
+      jest
+        .spyOn(UserRepository.prototype, 'FindUserByEmail')
+        .mockResolvedValue(null)
+
+      jest
+        .spyOn(UserRepository.prototype, 'CreateNewUser')
+        .mockResolvedValue({ _id: userPayload.user.id })
+
+      jest
+        .spyOn(UserRepository.prototype, 'UpdateUserToken')
+        .mockResolvedValue({
+          _id: userPayload.user.id,
+          email: userPayload.user.email
+        })
+
+      const response = await request(app)
+        .post('/signup')
+        .send({
+          user: {
+            email: userPayload.user.email,
+            password: userPayload.user.password
+          }
+        })
+
+      expect(response.statusCode).toBe(201)
+      expect(response.headers['set-cookie'][0].split(';')[0]).toContain('jwt=')
+      expect(
+        response.headers['set-cookie'][0].split(';')[0].split('jwt=')[1]
+      ).toBeTruthy()
+    })
+  })
+})
