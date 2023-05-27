a168a72ddd9e4bdbfcc093ab52b2d9568a9fcd56
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-27 17:55:50 +0100

    feat: setup project and express server

---
+++ project-service/src/index.js
@@ -0,0 +1,17 @@
+const { PORT } = require("./config");
+const expressApp = require("./express-app.js");
+
+const startServer = async () => {
+  // Express App
+  const app = expressApp();
+
+  app
+    .listen(PORT, () => {
+      console.log(`Example app listening at http://localhost:${PORT}`);
+    })
+    .on("error", (err) => {
+      console.log(err);
+    });
+};
+
+startServer();
