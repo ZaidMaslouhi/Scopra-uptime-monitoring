3c6a8361490477928dcaa5f08546d5f495f1b320
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-27 12:51:40 +0100

    refactor: rename user file to userApi

--- user-service/src/api/index.js
+++ user-service/src/api/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  user: require('./user')
+  userApi: require('./user')
 }
--- user-service/src/express-app.js
+++ user-service/src/express-app.js
@@ -1,5 +1,6 @@
 const cors = require('cors')
 const express = require('express')
+const { userApi } = require('./api')
 
 const expressApp = () => {
   const app = express()
@@ -8,6 +9,9 @@ const expressApp = () => {
   app.use(express.json({ limit: '1mb' }))
   app.use(cors({ credentials: true, origin: true }))
   app.use(express.urlencoded({ extended: true, limit: '1mb' }))
+
+  // Api
+  userApi(app)
 
   return app
 }
