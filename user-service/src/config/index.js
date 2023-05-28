6f2f9532b3dc4ab1901fdb4e68cc68d7b03118ca
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-05-28 01:21:07 +0100

    chore: add RPC events and channels

--- user-service/src/config/index.js
+++ user-service/src/config/index.js
@@ -1,5 +1,6 @@
 const Env = require('./envirement')
 const PassportConfig = require('./passport')
+const { UserServiceEvents } = require('./rpcEvents')
 const { RedisPublisher, RedisSubscriber } = require('./redis')
 
 module.exports = {
@@ -8,5 +9,11 @@ module.exports = {
   PassportConfig,
   // Redis
   RedisPublisher,
-  RedisSubscriber
+  RedisSubscriber,
+  // RPC services
+  USER_SERVICE: 'USER_SERVICE',
+  PROJECT_SERVICE: 'PROJECT_SERVICE',
+  MONITOR_SERVICE: 'MONITOR_SERVICE',
+  // RPC Events
+  UserServiceEvents
 }
---
+++ user-service/src/config/rpcEvents.js
@@ -0,0 +1,5 @@
+const UserServiceEvents = {
+  SET_DEFAULT_PROJECT: 'SET_DEFAULT_PROJECT'
+}
+
+module.exports = { UserServiceEvents }
