a0a6d9e6dd92113bc5639f993c4903ef1effc125
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-04 00:20:38 +0100

    feat: migrate from firebase to the project api

--- frontend/src/services/project.service.ts
+++ frontend/src/services/project.service.ts
@@ -1,32 +1,131 @@
-import {
-  collection,
-  addDoc,
-  getDocs,
-  deleteDoc,
-  doc,
-  updateDoc,
-} from "firebase/firestore";
-import { database } from "../config/firebase.config";
+import axios from "axios";
+import { AxiosError } from "./../../node_modules/axios/index.d";
 import { UserInfo } from "../interfaces/auth.interface";
 import { Project } from "../interfaces/project.interface";
+import { ServerError } from "../utils/interfaces/Error.interface";
 
-export const addNewProject = (user: UserInfo, project: Project) => {
-  const ref = collection(database, `user/${user.uid}/projects`);
-  return addDoc(ref, project);
+export const addNewProject = async (
+  user: UserInfo,
+  project: Project,
+  isDefault: boolean
+) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_BACKEND_API_PROJECT}/`,
+      {
+        user: { id: user.id },
+        project: {
+          name: project.name,
+          isDefault,
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (response.status === 201) {
+      return {
+        project: response.data["project"],
+        message: response.data["message"],
+      };
+    }
+    throw new Error("Somthing went wrong!");
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const serverError = error as AxiosError<ServerError>;
+      if (serverError && serverError.response) {
+        return serverError.response.data;
+      }
+    }
+    return { message: "Somthing went wrong!" };
+  }
 };
 
-export const getProjects = (user: UserInfo) => {
-  return getDocs(collection(database, `user/${user.uid}/projects`));
+export const getProjects = async (user: UserInfo) => {
+  try {
+    const response = await axios.get(
+      `${process.env.VITE_BACKEND_API_PROJECT}/`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response.status === 200) {
+      return {
+        projects: response.data["projects"],
+        defaultProject: response.data["defaultProject"],
+      };
+    }
+    throw new Error("Somthing went wrong!");
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const serverError = error as AxiosError<ServerError>;
+      if (serverError && serverError.response) {
+        return serverError.response.data;
+      }
+    }
+    return { message: "Somthing went wrong!" };
+  }
 };
 
-export const updateProject = (user: UserInfo, project: Project) => {
-  const projectDocument = doc(
-    database,
-    `user/${user.uid}/projects/${project.id}`
-  );
-  return updateDoc(projectDocument, { ...project });
+export const updateProject = async (user: UserInfo, project: Project) => {
+  try {
+    const response = await axios.put(
+      `${process.env.VITE_BACKEND_API_PROJECT}/`,
+      { project: { id: project.id, name: project.name } },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      return {
+        project: response.data["project"],
+        message: response.data["message"],
+      };
+    }
+    throw new Error("Somthing went wrong!");
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const serverError = error as AxiosError<ServerError>;
+      if (serverError && serverError.response) {
+        return serverError.response.data;
+      }
+    }
+    return { message: "Somthing went wrong!" };
+  }
 };
 
-export const removeProject = (user: UserInfo, project: Project) => {
-  return deleteDoc(doc(database, `user/${user.uid}/projects/${project.id}`));
+export const removeProject = async (user: UserInfo, project: Project) => {
+  try {
+    const response = await axios.delete(
+      `${process.env.VITE_BACKEND_API_PROJECT}/${project.id}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      return {
+        project: response.data["project"],
+      };
+    }
+    throw new Error("Somthing went wrong!");
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const serverError = error as AxiosError<ServerError>;
+      if (serverError && serverError.response) {
+        return serverError.response.data;
+      }
+    }
+    return { message: "Somthing went wrong!" };
+  }
 };
