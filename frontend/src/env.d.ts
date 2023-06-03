cc3115b89e193011eb27104e5e4c09d3f4ad1569
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-03 22:51:29 +0100

    chore: add env to env.d.ts interface

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,9 +1,24 @@
 interface ImportMetaEnv {
-    readonly VITE_TOKEN: string;
-    readonly VITE_CLIENT_ID: number;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-  
+  readonly NODE_ENV: "development" | "production";
+  readonly VITE_TOKEN: string;
+  readonly VITE_CLIENT_ID: number;
+
+  readonly VITE_WEBSOCKET_URI: string;
+
+  readonly VITE_BACKEND_API_USER: string;
+  readonly VITE_BACKEND_API_PROJECT: string;
+  readonly VITE_BACKEND_API_MONITOR: string;
+
+  readonly VITE_GITHUB_API_KEY: string;
+
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
