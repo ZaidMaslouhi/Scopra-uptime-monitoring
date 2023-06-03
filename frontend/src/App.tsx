30774d038edca48db7f6890b3d760953a6903d56
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-04 00:22:25 +0100

    feat: deconnect from ws on re-rendering the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,22 @@ import { Route, Routes } from "react-router-dom";
 import { routes } from "./utils/routes/routes";
 import { Toaster } from "react-hot-toast";
 import LoadingAnimation from "./components/ui/loading/LoadingAnimation";
-import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./config/firebase.config";
 import { useAppDispatch } from "./utils/hooks/react-redux-hooks";
-import { saveUser, verifyUser } from "./store/slices/auth.slice";
-import { toUserInfo } from "./interfaces/auth.interface";
+import { refreshUserToken } from "./store/slices/auth.slice";
+import { disconnectWebSocket } from "./store/slices/websocket.actions";
 
 function App() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(verifyUser());
-    onAuthStateChanged(auth, (user) => {
-      dispatch(saveUser(toUserInfo(user)));
-    });
+    // dispatch(refreshUserToken());
+    // onAuthStateChanged(auth, (user) => {
+    //   dispatch(saveUser(toUserInfo(user)));
+    // });
+    return () => {
+      dispatch(disconnectWebSocket());
+    };
   }, [auth, dispatch]);
 
   return (
